f15412d4b139e198b46dc103f4d334befe4d9d5b
BrunoSantosdeOliveira <daniel-s-oliveira@hotmail.com>
2022-11-15 21:11:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using PropriedadesMetodos.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Daniel";
+p1.Sobrenome = "Oliveira";
+//p1.Idade = 31;
+//p1.Apresentar();
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Daniel";
+p2.Sobrenome = "Oliveira";
+
+
+
