f94b29fd76ffe0ecdbb220a3f15ca3a8df458acd
BrunoSantosdeOliveira <daniel-s-oliveira@hotmail.com>
2022-11-15 21:48:38 -0300

    Construtor

--- Program.cs
+++ Program.cs
@@ -1,14 +1,11 @@
 ﻿using PropriedadesMetodos.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Daniel";
-p1.Sobrenome = "Oliveira";
+Pessoa p1 = new Pessoa(nome: "Daniel",sobrenome: "Oliveira");
 //p1.Idade = 31;
 //p1.Apresentar();
 
-Pessoa p2 = new Pessoa();
-p2.Nome = "Bruno";
-p2.Sobrenome = " Santos de Oliveira";
+Pessoa p2 = new Pessoa(nome: "Bruno",sobrenome: " Santos de Oliveira");
+
 
 Curso cursoDeIngles = new Curso();
 cursoDeIngles.Nome = "Ingles";
