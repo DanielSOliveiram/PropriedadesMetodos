e0039be5dc08dcf551e7555f81e1785345433f2c
BrunoSantosdeOliveira <daniel-s-oliveira@hotmail.com>
2022-11-22 15:06:23 -0300

    StringCocatenacaoEInterpolacao

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 ﻿using PropriedadesMetodos.Models;
 
-Pessoa p1 = new Pessoa(nome: "Daniel",sobrenome: "Oliveira");
+string numero1 = "10";
+int numero2 =20;
+
+string resultado = numero1 + numero2;
+
+Console.WriteLine(resultado);
+
+
+
+
+/*Pessoa p1 = new Pessoa(nome: "Daniel",sobrenome: "Oliveira");
 //p1.Idade = 31;
 //p1.Apresentar();
 
@@ -13,7 +23,8 @@ cursoDeIngles.Alunos = new List<Pessoa>();
 
 cursoDeIngles.AdicionarAluno(p1);
 cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+cursoDeIngles.ListarAlunos();*/
+
 
 
 
