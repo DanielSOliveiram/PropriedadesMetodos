67cff2d819d49a6154e383e574768f591c7926e6
BrunoSantosdeOliveira <daniel-s-oliveira@hotmail.com>
2022-12-12 00:02:56 -0300

    eee

--- Program.cs
+++ Program.cs
@@ -3,7 +3,23 @@ using PropriedadesMetodos.Models;
 using System.Globalization;
 
 
-new ExemploExcecao().Metodo1();
+Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+
+foreach(int  item in fila)
+{
+    Console.WriteLine(item);
+}
+
+fila.Dequeue();
+
+Stack<int> pilha = new Stack<int>();
+
+//new ExemploExcecao().Metodo1();
 
 
 
